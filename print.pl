5229cc13062b6899d7685e64c979bb3c9139d008
Audrey Tang <audreyt@audreyt.org>
2012-08-13 14:41:51 +0800

    * Pause after 10 batches to avoid COM3 buffer overflow

--- print.pl
+++ print.pl
@@ -1,19 +1,32 @@
 #!/usr/bin/env perl
 use utf8;
+use 5.12.0;
 use DateTime;
 use Business::TW::Invoice::U420;
 use Encode 'encode';
 binmode STDIN, ':utf8';
-my @tickets = split /\n\.\n/, join '', <>;
-open my $com3, '>', 'COM3';
-my $u420 = Business::TW::Invoice::U420->new(
-    { heading => [],
-        lines_total => 35,
-        lines_available => 22,
-        lines_stamp => 5,
-        fh => $com3 });
-$u420->init_printer;
-for my $t (@tickets) {
-    $u420->println(encode(big5 => $_)) for split(/\n/, $t);
-    $u420->cut;
+my @all = split /\n\.\n/, join '', <>;
+while (@all) {
+    my @tickets = splice(@all, 0, 10);
+
+    open my $com3, '>', 'COM3';
+    my $u420 = Business::TW::Invoice::U420->new(
+        { heading => [],
+            lines_total => 35,
+            lines_available => 22,
+            lines_stamp => 5,
+            fh => $com3 });
+    $u420->init_printer;
+    for my $t (@tickets) {
+        $u420->println(encode(big5 => $_)) for split(/\n/, $t);
+        $u420->cut;
+    }
+    close $com3;
+
+    open my $fh, '>:raw', 'rest.txt';
+    print $fh join("\n.\n", @all);
+    close $fh;
+    sleep 105;
 }
+
+say "All done!";
